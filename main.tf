653cee6bd2dceca4f6be3dc1403536fe93928a8a
joeldesaulniers <60636149+joeldesaulniers@users.noreply.github.com>
2020-09-08 20:55:36 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "remote" {
+    organization = "joelcorp"
+
+    workspaces {
+      name = "joel-workspace"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ca-central-1"
+}
+
+module "notify_slack" {
+  source  = "terraform-aws-modules/notify-slack/aws"
+  version = "~> 4.0"
+
+  sns_topic_name = "slack-topic"
+
+  slack_webhook_url = "https://hooks.slack.com/services/AAA/BBB/CCC"
+  slack_channel     = "aws-notification"
+  slack_username    = "reporter"
+}
