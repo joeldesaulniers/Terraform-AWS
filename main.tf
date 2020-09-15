b08a4ba609ad2419443ed80770bb4e1927b84cc7
mail@joeldesaulniers.com <jdesau@f0189809be1c.ant.amazon.com>
2020-09-15 07:56:29 -0400

    adding instance

--- main.tf
+++ main.tf
@@ -12,9 +12,13 @@ provider "aws" {
   region  = "ca-central-1"
 }
 
-/*
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ssm_profile"
+  role = "EC2-SSM"
+}
+
 resource "aws_instance" "example" {
-  ami 			= "ami-020caff809d5a5307"
+  ami 			= "ami-0c94855ba95c71c99"
   instance_type = "t3.nano"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
 } 
-*/
